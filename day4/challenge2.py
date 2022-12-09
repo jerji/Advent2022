059274bd2c6e7ced03bf49ecbc6189f8f1032469
Jeremy Ouellet <jouellet@beanfield.com>
2022-12-09 15:34:10 -0500

    Completed Day 4

---
+++ day4/challenge2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+total = 0
+
+with open('input.txt') as f:
+    for line in f:
+        ranges = line.strip().split(',')
+        range1ends = ranges[0].split('-')
+        range2ends = ranges[1].split('-')
+        range1 = {*range(int(range1ends[0]),int(range1ends[1])+1)}
+        range2 = {*range(int(range2ends[0]),int(range2ends[1])+1)}
+        intersec = range1.intersection(range2)
+        if intersec != set():
+            print(intersec,range1,range2)
+            total +=1
+
+print(total)
